a16a39466ad399e728ba8855ee751ddb5046dd3e
Brendon Amino <101315240+BrenAmin1@users.noreply.github.com>
2024-10-11 19:14:45 +0000

    Pending changes exported from your codespace

--- assignment6/wumpus.rb
+++ assignment6/wumpus.rb
@@ -21,47 +21,60 @@ class MyTest
 	THIS_IS_A_CONSTANT = "IM A CONSTANT"
 end
 
+
 class Tile
-	#this is how we define a class method
-	def type
-		"O"
+	attr_accessor :tileAction
+	@tileAction = ""
+	def SetAction(action)
+		@tileAction = action
+	end
+
+	def GetAction
+		@tileAction
 	end
 end
 
-#Derived1 inherits from Base
+class EmptyTile < Tile
+	def type
+		"O"
+	end
+	
+	
+end
+
 class Pit < Tile
-	#type is an example of a polymorphic method
 	def type 
 		"P"
 	end
 end
 
-#Derived2 also inherits from Base
 class Wumpus < Tile
-	#type is an example of a polymorphic method
 	def type 
 		"W"
 	end
 end	
 
+
 #The Composition class is composed of objects from Base, Derived1, and Derived2
 class Composition
-	attr_accessor :maze; 	#this is how you create a class variable (attribute) in Ruby
-				#arr is encapsulated inside of the Composition class
-	#initialize() is equivalent to a constuctor in C++
+	attr_accessor :maze 
+	attr_accessor :playerRow
+	attr_accessor :playerCol
 	def initialize()
 		@maze = [	
-        [Tile.new, Tile.new, Tile.new, Tile.new, Tile.new, Tile.new], 
-        [Tile.new, Tile.new, Tile.new, Tile.new, Tile.new, Tile.new],
-        [Tile.new, Tile.new, Tile.new, Tile.new, Tile.new, Tile.new], 
-        [Tile.new, Tile.new, Tile.new, Tile.new, Tile.new, Tile.new]
+        [EmptyTile.new, EmptyTile.new, EmptyTile.new, EmptyTile.new, EmptyTile.new, EmptyTile.new], 
+        [EmptyTile.new, EmptyTile.new, EmptyTile.new, EmptyTile.new, EmptyTile.new, EmptyTile.new],
+        [EmptyTile.new, EmptyTile.new, EmptyTile.new, EmptyTile.new, EmptyTile.new, EmptyTile.new], 
+        [EmptyTile.new, EmptyTile.new, EmptyTile.new, EmptyTile.new, EmptyTile.new, EmptyTile.new]
     ]
+		@playerRow = 3
+		@playerCol = 0
 		#NOTE: In methods you put the @ symbol in front of member variables
 		#This is how Ruby knows it is the member variable and not a local variable
 	end
 
 	#prints the contents of arr to the screen
-	def printArray
+	def printMaze
 		for r in 0..3 do
 			for c in 0..5 do
 				print "#{@maze[r][c].type} "
@@ -70,14 +83,63 @@ class Composition
 		end
 	end
 
-	#prints the object in arr at row and col
-	def objectAtIn2Darray(row, col)
-		if row >= 0 and row < 2 and col >= 0 and col < 2
-			puts "Item at (#{row},#{col}) is #{@arr[row][col].type}"
-		else
-			puts "That row and column is out of bounds!"
+	def printMap
+		for r in 0..3 do
+			for c in 0..5 do
+				if r == @playerRow and c == @playerCol
+					print "X"
+				else
+					print "O"
+				end
+			end
+		puts ""
 		end
 	end
+
+	def playerMove(letter)
+		if letter == 'u' and @playerRow > 0
+			@playerRow -= 1 #Move up	
+		elsif letter == 'd' and @playerRow < 3
+			@playerRow += 1 #Move down
+		elsif letter == 'l' and @playerCol > 0
+			@playerCol -= 1 #Move left
+		elsif letter == 'r' and @playerCol < 5
+			@playerCol += 1 #Move right
+		else
+			print "\nOuch! You ran into a wall\n" #Collision with wall
+		end
+		checkMove(@playerRow, @playerCol)
+	end
+
+	def checkMove(row, col)
+		if row > 0
+			print @maze[row - 1][col].GetAction
+		end
+		# check for bottom tile
+		if row < 3
+			print @maze[row + 1][col].GetAction
+		end
+		# check for left tile
+		if col > 0
+			print @maze[row][col - 1].GetAction
+		end
+		# check for right tile
+		if col < 5
+			print @maze[row][col + 1].GetAction
+		end
+	end
+
+	def playerShoot(letter)
+		
+
+	# #prints the object in arr at row and col
+	# def objectAtIn2Darray(row, col)
+	# 	if row >= 0 and row < 2 and col >= 0 and col < 2
+	# 		puts "Item at (#{row},#{col}) is #{@arr[row][col].type}"
+	# 	else
+	# 		puts "That row and column is out of bounds!"
+	# 	end
+	# end
 
 	# Initialize the maze by randomly placing pits and a wumpus
 	def initMaze
@@ -85,6 +147,7 @@ class Composition
 		for r in 0..3
 			col = rand(1..5) #rand(2) generates a random number from 0 to 1
 			@maze[r][col] = Pit.new
+			addActionsToTile(r, col, "P")
 		end
 		# Places the wumpus
 		row = rand(0..3)
@@ -94,14 +157,51 @@ class Composition
 			col = rand(1..5)
 		end
 		@maze[row][col] = Wumpus.new
+		addActionsToTile(row, col, "W")
+	end
+
+	# adds actions to tile based on wampus and pits position
+	def addActionsToTile(row, col, type)
+		# check for top tile
+		if row > 0
+			if type == "P"
+				@maze[row - 1][col].SetAction("You feel a breeze...\n")
+			else 
+				@maze[row - 1][col].SetAction("You smell a stench...\n")
+			end	
+		end
+		# check for bottom tile
+		if row < 3
+			if type == "P"
+				@maze[row + 1][col].SetAction("You feel a breeze...\n")
+			else 
+				@maze[row + 1][col].SetAction("You smell a stench...\n")
+			end	
+		end
+		# check for left tile
+		if col > 0
+			if type == "P"
+				@maze[row][col - 1].SetAction("You feel a breeze...\n")
+			else 
+				@maze[row][col - 1].SetAction("You smell a stench...\n")
+			end	
+		end
+		# check for right tile
+		if col < 5
+			if type == "P"
+				@maze[row][col + 1].SetAction("You feel a breeze...\n")
+			else 
+				@maze[row][col + 1].SetAction("You smell a stench...\n")
+			end	
+		end
 	end
 
 	def doStuff
 		choice = "O"
 		while choice != "q"
-			printArray
+			printMaze
 			puts ""
-			puts "type do you want to do:"
+			puts "What do you want to do:"
 			puts " u) Move Up"
 			puts " d) Move Down"
 			puts " l) Move Left"
@@ -111,7 +211,7 @@ class Composition
 			print "ENTER CHOICE: "
 			choice = gets.chomp #.chomp removes the newline from the input
 			if choice == "s"
-				puts "type direction do you want to shoot your arrow: "
+				puts "What direction do you want to shoot your arrow:"
 				puts " u) Shoot Up"
 				puts " d) Shoot Down"
 				puts " l) Shoot left"
@@ -137,16 +237,20 @@ class Composition
 				
 			elsif choice == "u"
 				# move character up
-				randomObjectIn2Darray
+				playerMove(choice)
+				printMap
 			elsif choice == "d"
 				# move character down
-				printArray
+				playerMove(choice)
+				printMap
 			elsif choice == "l"
 				# move character left
-				printArray
+				playerMove(choice)
+				printMap
 			elsif choice == "r"
 				# move character right
-				printArray
+				playerMove(choice)
+				printMap
 			elsif choice == ""
 				puts "Invalid choice!"
 			else
@@ -158,5 +262,6 @@ end
 
 #this is the main program body
 c = Composition.new #create a new Composition object called c
+c.printMap
 c.initMaze
 c.doStuff #call the method doStuff of object c
